34d1aa5d5040e8ef9024c90f1d0d659dca35ea08
Dewmin Deniyegedara <137167677+DewminK@users.noreply.github.com>
2024-05-05 21:46:47 +0530

    Update appointment.php
    Added the form's data to store in the database with js form validations

--- appointment.php
+++ appointment.php
@@ -1,3 +1,41 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "hospital_appointments";
+
+    
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    
+    $stmt = $conn->prepare("INSERT INTO appointments (first_name, last_name, age, telephone_number, appointment_date, doctor) VALUES (?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("ssisss", $first_name, $last_name, $age, $telephone_number, $appointment_date, $doctor);
+
+    // Set parameters and execute statement
+    $first_name = $_POST['fname'];
+    $last_name = $_POST['lname'];
+    $age = $_POST['age'];
+    $telephone_number = $_POST['tpno'];
+    $appointment_date = $_POST['appointment_date'];
+    $doctor = $_POST['doctor'];
+    $stmt->execute();
+
+    // Close statement and database connection
+    $stmt->close();
+    $conn->close();
+
+    // Redirect or show success message
+    echo "<script>alert('Appointment successfully scheduled!');</script>";
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,6 +51,32 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <script src="https://kit.fontawesome.com/6eb5927010.js" crossorigin="anonymous"></script>
     <title>Appointments</title>
+    <script>
+        function validateForm(form) {
+            var fname = form.fname.value;
+            var lname = form.lname.value;
+            var age = form.age.value;
+            var tpno = form.tpno.value;
+            var appointment_date = form.appointment_date.value;
+            var doctor = form.doctor.value;
+
+            if (fname == "" || lname == "" || age == "" || tpno == "" || appointment_date == "" || doctor == "") {
+                alert("Please fill in all fields.");
+                return false;
+            }
+            if(0>age || age>100){
+              alert("Invalid age");
+              return false;
+            }
+            var format = /^\d{10}$/; 
+            if (!format.test(tpno)) {
+              alert("Invalid phone number.");
+              return false;
+            }
+
+            return true;
+        }
+    </script>
 </head>
 <body>
     <!-----------------------Navigation bar and Logo------------------------>
@@ -61,47 +125,52 @@
     </div>
 
     <div class="container appointment-form">
-      <div class="row">
-        <form action="" class="contact" Method="POST">
-          <div class="form-group">
-            <label for="fname">First Name</label>
-            <input type="text" id="fname" name="fname" class="fname form-control">
-          </div>
-          <div class="form-group">
-            <label for="lname">Last Name</label>
-            <input type="text" id="lname" name="lname" class="lname form-control">
-          </div>
-          <div class="form-group">
-            <label for="age">Age</label>
-            <input type="text" id="age" name="age" class="age form-control">
-          </div>
-          
-          <div class="form-group">
-            <label for="tpno">Telephone Number</label>
-            <input type="text" id="tpno" name="tpno" class="tpno form-control">
-          </div>
-          <div class="form-group">
-            <label for="age">Select Date</label>
-            <input type="date" id="age" name="age" class="age form-control">
-          </div>
-          <div class="form-group">
-            <label for="age">Select Doctor</label>
-            <select name="" id="" class="form-control">
-              <option value="" selected="selected">Select the Doctor</option>
-              <option value="">Dr. Pasindu Lakshan</option>
-              <option value="">Dr. Thilina Heashan</option>
-              <option value="">Dr. Kavindu Kaushal</option>
-              <option value="">Dr. Chamoda Sankalpani</option>
-            </select>
-          </div>
-          <div class="form-group">
-           <a href="" class="btn btn-primary">Make Appointment</a>
-           <a href="" class="btn btn-primary">Clear</a>
-          </div>
-          
+    <div class="row">
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" class="contact" onsubmit="return validateForm(this)">
+            <div class="form-group">
+                <label for="fname">First Name</label>
+                <input type="text" id="fname" name="fname" class="fname form-control">
+            </div>
+            <div class="form-group">
+                <label for="lname">Last Name</label>
+                <input type="text" id="lname" name="lname" class="lname form-control">
+            </div>
+            <div class="form-group">
+                <label for="age">Age</label>
+                <input type="text" id="age" name="age" class="age form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="tpno">Telephone Number</label>
+                <input type="text" id="tpno" name="tpno" class="tpno form-control">
+            </div>
+            <div class="form-group">
+                <label for="appointment_date">Select Date</label>
+                <input type="date" id="appointment_date" name="appointment_date" class="appointment_date form-control">
+            </div>
+            <div class="form-group">
+                <label for="doctor">Select Doctor</label>
+                <select name="doctor" id="doctor" class="form-control">
+                    <option value="" selected="selected">Select the Doctor</option>
+                    <option value="Dr. S.M.D Silva">Dr. S.M.D Silva</option>
+                    <option value="Dr. Dr. R.P.S Kumara">Dr. R.P.S Kumara</option>
+                    <option value="Dr. L.M Hafeez">Dr. L.M Hafeez</option>
+                    <option value="Dr. Nimal Rajapaksha">Dr. Nimal Rajapaksha</option>
+                    <option value="Dr. P.I Chandrasiri">Dr. Nimal Rajapaksha</option>
+                    <option value="Dr. Pasindu Lakshan">Dr. Pasindu Lakshan</option>
+                    <option value="Dr. Thilina Heashan">Dr. Thilina Heashan</option>
+                    <option value="Dr. Kavindu Kaushal">Dr. Kavindu Kaushal</option>
+                    <option value="Dr. Chamoda Sankalpani">Dr. Chamoda Sankalpani</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <button type="submit" name="submit" class="btn btn-primary">Make Appointment</button>
+                <button type="reset" class="btn btn-primary">Clear</button>
+            </div>
         </form>
-       </form>
-      </div>
+    </div>
+</div>
+
     </div>
 
     <footer>
@@ -143,4 +212,3 @@
     </footer>
   </body>
 </html>
-  
