085b0dec8a3935740dbb156de83b39aceb6409ec
Dewmin Deniyegedara <137167677+DewminK@users.noreply.github.com>
2024-05-05 12:37:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -109,7 +109,7 @@
           <h1>Welcome to<br>MT Hospital</h1>
           <h3>The Largest health care center</h3>
           <a href="#" class="btn btn-primary banbtn">Join with Us</a>
-          <a href="#" class="btn btn-primary banbtn">Make Appointment</a>
+          <a href="appointment.php" class="btn btn-primary banbtn">Make Appointment</a>
         </div>
       </div>
       <div class="col-sm-6">
@@ -216,7 +216,7 @@
           <span class="sr-only">Next</span>
         </a>
       </div>
-      <a href="facility.html" class="show-more btn btn-primary text-center">Show all Facilities &nbsp; <i class="fa-sharp fa-solid fa-circle-arrow-right"></i></a>
+      <a href="facility.php" class="show-more btn btn-primary text-center">Show all Facilities &nbsp; <i class="fa-sharp fa-solid fa-circle-arrow-right"></i></a>
     </div>
   </div>
 
